1e6009dfa6011bfda633ba5ccaf7419f9e910ca7
KEANU.WONG <386247915@qq.com>
2020-07-22 21:10:36 +0800

    [KEANU] featured: edit the practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private String displayName;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Klass(int number){
+        this.number=number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader=student;
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public Klass getKlass(){
+        return this;
+    }
 }
